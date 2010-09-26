423ff23e3410ca2989b094f75a11f5327eef477c
shlomif <shlomif@b384bcd7-cfd4-0310-aca0-d78b80f7b91b>
2010-09-26 21:12:12 +0000

    Branch fc-solve into the "rcs-states-storage" branch.
    This is to add support for:
    
    http://fc-solve.berlios.de/to-do.html#rcs_state_storage

---
+++ branches/rcs-states-storage/fc-solve/source/scripts/pgo.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This is a script for PGO - Profile Guided Optimisations in gcc.
+
+compiler="$1"
+shift
+mode="$1"
+shift
+
+pgo_flags=""
+make_vars=()
+
+if test "$mode" = "use" ; then
+    if test "$compiler" = "gcc" ; then
+        pgo_flags="-fprofile-use"
+    elif test "$compiler" = "icc" ; then
+        pgo_flags="-prof-use"
+    else
+        echo "Unknown compiler '$compiler'!" 1>&2
+        exit -1
+    fi
+    make_vars=(OPT_AND_DEBUG=0 DEBUG=0)
+elif test "$mode" = "gen" ; then
+    if test "$compiler" = "gcc" ; then
+        pgo_flags="-fprofile-generate"
+    elif test "$compiler" = "icc" ; then
+        pgo_flags="-prof-gen"
+    else
+        echo "Unknown compiler '$compiler'!" 1>&2
+        exit -1
+    fi
+else
+    echo "Unknown mode '$mode'!" 1>&2
+    exit -1
+fi
+    
+make -f Makefile.gnu FREECELL_ONLY=1 \
+    EXTRA_CFLAGS="$pgo_flags" \
+    COMPILER="$compiler" \
+    $make_vars
